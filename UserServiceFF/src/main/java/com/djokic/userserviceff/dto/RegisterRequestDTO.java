1bb50fefa6fa6c87c2c465ea780936111b5ab92a
Alex <62123288+caojasamalex@users.noreply.github.com>
2025-09-22 19:49:47 +0200

    Remove validation annotations from RegisterRequestDTO
    Validation constraints were removed from RegisterRequestDTO fields, making the DTO a plain data object. Also, the UserController's base request mapping was changed from '/api/users' to '/users'.

--- UserServiceFF/src/main/java/com/djokic/userserviceff/dto/RegisterRequestDTO.java
+++ UserServiceFF/src/main/java/com/djokic/userserviceff/dto/RegisterRequestDTO.java
@@ -1,9 +1,5 @@
 package com.djokic.userserviceff.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -14,22 +10,12 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class RegisterRequestDTO {
-
-    @NotBlank(message = "E-Mail must be provided !")
-    @Pattern(
-            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-            message = "Email must be a valid email address with no spaces"
-    )
     private String email;
 
-    @NotBlank(message = "Password cannot be blank")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
-    @NotBlank(message = "First name cannot be blank")
     private String firstName;
 
-    @NotBlank(message = "Last name cannot be blank")
     private String lastName;
 
     public String getEmail() {
