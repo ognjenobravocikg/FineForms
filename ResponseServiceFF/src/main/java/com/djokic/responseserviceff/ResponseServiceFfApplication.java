6389cb35051f44a156042641bc944fa783d17bac
Alex <62123288+caojasamalex@users.noreply.github.com>
2025-09-25 22:57:22 +0200

    Add dotenv support for database configuration
    Introduced dotenv-java dependency and .env file to manage database credentials. Updated application startup to load environment variables and configured Spring datasource properties to use these variables for PostgreSQL connection.

--- ResponseServiceFF/src/main/java/com/djokic/responseserviceff/ResponseServiceFfApplication.java
+++ ResponseServiceFF/src/main/java/com/djokic/responseserviceff/ResponseServiceFfApplication.java
@@ -1,5 +1,6 @@
 package com.djokic.responseserviceff;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResponseServiceFfApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("RESPONSE_DB_URL", dotenv.get("RESPONSE_DB_URL"));
+        System.setProperty("RESPONSE_DB_USERNAME", dotenv.get("RESPONSE_DB_USERNAME"));
+        System.setProperty("RESPONSE_DB_PASSWORD", dotenv.get("RESPONSE_DB_PASSWORD"));
+
         SpringApplication.run(ResponseServiceFfApplication.class, args);
     }
 
