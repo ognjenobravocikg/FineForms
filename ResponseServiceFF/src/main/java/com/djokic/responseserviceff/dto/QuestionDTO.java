cd1451b9feb20b6deff136e2b0fb7aa8fc65bf1c
Kenza <jankokg2002@gmail.com>
2025-09-27 02:15:16 +0200

    Fixed questions not being properly sent
    Added Jsonignore annotation so the response creation works properly in all cases

--- ResponseServiceFF/src/main/java/com/djokic/responseserviceff/dto/QuestionDTO.java
+++ ResponseServiceFF/src/main/java/com/djokic/responseserviceff/dto/QuestionDTO.java
@@ -1,7 +1,9 @@
 package com.djokic.responseserviceff.dto;
 
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class QuestionDTO {
     private Long id;
@@ -9,4 +11,5 @@ public class QuestionDTO {
     private String type; // text, number, multiple_choice, etc.
     private boolean required;
     private int position;
+    private Boolean requiredQuestion;
 }
